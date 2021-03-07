3d51376f96cd78e2782f2010cc7b164e9eebce23
Hesham <hesham.elfaham@gmail.com>
2021-03-07 15:35:15 +0200

    small updates

--- run_lhe_analyzer.sh
+++ run_lhe_analyzer.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 gunzip tth_sm/Events/run_02/unweighted_events.lhe.gz
+#gunzip tth_sm/Events/run_04/unweighted_events.lhe.gz
 
-gunzip tth_smeft/Events/run_07/unweighted_events.lhe.gz
-#gunzip tth_smeft_prod_only/Events/run_01/unweighted_events.lhe.gz
+#gunzip tth_smeft/Events/run_07/unweighted_events.lhe.gz
+gunzip tth_smeft/Events/run_10/unweighted_events.lhe.gz
 
 gunzip tth_smeft/Events/run_08/unweighted_events.lhe.gz
 
@@ -10,9 +11,10 @@ gunzip tth_smeft/Events/run_09/unweighted_events.lhe.gz
 
 #python
 python lhe_analyzer_decayed.py tth_sm/Events/run_02/unweighted_events.lhe ./root_files/sm_tth.root
+#python lhe_analyzer_decayed.py tth_sm/Events/run_04/unweighted_events.lhe ./root_files/sm_tth.root
 
-python lhe_analyzer_decayed.py tth_smeft/Events/run_07/unweighted_events.lhe ./root_files/smeft_tth_wilson_zero.root
-#python lhe_analyzer_decayed.py tth_smeft_prod_only/Events/run_01/unweighted_events.lhe ./root_files/smeft_tth_wilson_zero.root
+#python lhe_analyzer_decayed.py tth_smeft/Events/run_07/unweighted_events.lhe ./root_files/smeft_tth_wilson_zero.root
+python lhe_analyzer_decayed.py tth_smeft/Events/run_10/unweighted_events.lhe ./root_files/smeft_tth_wilson_zero.root
 
 python lhe_analyzer_decayed.py tth_smeft/Events/run_08/unweighted_events.lhe ./root_files/smeft_tth_wilson_cpt.root
 
